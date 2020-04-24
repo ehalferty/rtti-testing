a408f5f4e809b45bc98d95080f8ab51d4360cbb8
Ed Halferty <edhalferty@gmail.com>
2020-04-24 00:23:42 -0700

    blah

--- main.cpp
+++ main.cpp
@@ -18,12 +18,24 @@ public:
 class XyFooFoo {
 public:
     static inline XyRTTI * rtti = new XyRTTI("XyFooFoo", XyFoo::rtti);
+    bool IsKindOf(XyRTTI const * other) {
+        XyRTTI const * next = rtti;
+        while (next) {
+            if (next == rtti) {
+                return true;
+            }
+            next = next->base;
+        }
+        return false;
+    }
 };
 
 int main() {
     XyFoo xyFoo;
     XyFooFoo xyFooFoo;
     std::cout << "Class of xyFoo: " << xyFoo.rtti->name << std::endl <<
-        "Class of xyFooFoo: "<< xyFooFoo.rtti->name << std::endl;
+        "Class of xyFooFoo: "<< xyFooFoo.rtti->name << std::endl <<
+        "And xyFooFoo IS" << (xyFooFoo.IsKindOf(xyFoo.rtti) ? "" : " NOT") <<
+        " a type of xyFoo" << std::endl;
     return 0;
 }
